fa73d603af463bb1cb1890573cf596e976e9ef17
John <jiangquan1979@gmail.com>
2014-04-03 22:23:06 +0800

    Replace https with http when using CDN domain

--- NRGramKit/Model/IGImage.m
+++ NRGramKit/Model/IGImage.m
@@ -39,9 +39,17 @@ static NSString* nonCDNDomain;
     NSDictionary* standardDictionary = [dict objectForKey:@"standard_resolution"];
     NSDictionary* lowDictionary = [dict objectForKey:@"low_resolution"];
 
-    image.low_resolution = [self shouldReplaceCDNDomain] ? [[lowDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[lowDictionary objectForKey:@"url"];
-    image.standard_resolution = [self shouldReplaceCDNDomain] ? [[standardDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[standardDictionary objectForKey:@"url"];
-    image.thumbnail = [self shouldReplaceCDNDomain] ? [[thumbnailDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[thumbnailDictionary objectForKey:@"url"];
+    BOOL useCDN = [self shouldReplaceCDNDomain];
+    image.low_resolution = useCDN ? [[lowDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[lowDictionary objectForKey:@"url"];
+    image.standard_resolution = useCDN ? [[standardDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[standardDictionary objectForKey:@"url"];
+    image.thumbnail = useCDN ? [[thumbnailDictionary objectForKey:@"url"] stringByReplacingOccurrencesOfString:nonCDNDomain withString:cdnDomain]:[thumbnailDictionary objectForKey:@"url"];
+    
+    if(useCDN) {
+        // change https to http when using cdn
+        image.low_resolution = [image.low_resolution stringByReplacingOccurrencesOfString:@"https://" withString:@"http://"];
+        image.thumbnail = [image.thumbnail stringByReplacingOccurrencesOfString:@"https://" withString:@"http://"];
+        image.standard_resolution = [image.standard_resolution stringByReplacingOccurrencesOfString:@"https://" withString:@"http://"];
+    }
     
     return image;
 }
